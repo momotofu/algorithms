91e8a37eb0600fa8ddccea7eefd169498d758229
Christopher Reece <mrkita.reece@gmail.com>
2017-12-02 13:03:13 +0900

    added init function

--- graph_theory/graph.py
+++ graph_theory/graph.py
@@ -3,4 +3,15 @@ A simple Python graph class, demonstrating the essential
 facts and functionalities of graphs.
 """
 
+class Graph(object):
+    def __init__(self, graph_dict=None):
+        """
+        Initializes the graph object.
+        If no dictionary or None is given,
+        an empty dictionary will be used.
+        """
+        if graph_dict == None:
+            graph_dict = {}
+        self.graph_dict = graph_dict
+
 
