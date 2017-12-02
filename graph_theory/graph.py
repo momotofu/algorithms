76b5d00ec99d7121e01829da8af0946daec0ad9b
Christopher Reece <mrkita.reece@gmail.com>
2017-12-02 13:50:50 +0900

    added find_path method to graph class

--- graph_theory/graph.py
+++ graph_theory/graph.py
@@ -47,6 +47,25 @@ class Graph(object):
         else:
             self.__graph_dict[vertex1] = [vertex2]
 
+    def find_path(self, start_vertex, end_vertex, path=None):
+        """
+        find a path from start_vertex to end_vertex in graph
+        """
+        if path == None:
+            path = []
+        graph = self.__graph_dict
+        path = path + start_vertex
+        if start_vertex == end_vertex:
+            return path
+        if start_vertex not in graph:
+            return None
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_path = self.find_path(vertex, end_vertex, path)
+                if extended_path:
+                    return extended_path
+        return None
+
     def __generate_edges(self):
         """
         A static method generating the edges of the graph "graph".
@@ -67,4 +86,4 @@ class Graph(object):
         res += "\nedges:"
         for edge in self.__generate_edges():
             res += str(edge) + " "
-        return res
+        return print(res)
