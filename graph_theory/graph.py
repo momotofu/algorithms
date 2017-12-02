eceeec9ddeb694377de42424313791f0b83fda50
Christopher Reece <mrkita.reece@gmail.com>
2017-12-02 13:24:18 +0900

    filled out graph class

--- graph_theory/graph.py
+++ graph_theory/graph.py
@@ -12,6 +12,59 @@ class Graph(object):
         """
         if graph_dict == None:
             graph_dict = {}
-        self.graph_dict = graph_dict
+        self.__graph_dict = graph_dict
 
+    def vertices(self):
+        """
+        returns the vertices of a graph
+        """
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        """
+        returns a list of edges in the graph
+        """
+        return self.__generate_edges()
+
+    def add_vertex(self, vertix):
+        """
+        If the vertex is not in the self.__graph_dict, a key
+        "vertex" with an empty list as a value is added to
+        the dictionary, otherwise nothing has to be done.
+        """
+        if vertex not in self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        """
+        assumes that the edge is of type set, tuple, or list;
+        between two vertices can be multiple edges.
+        """
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        """
+        A static method generating the edges of the graph "graph".
+        Edges are reprsented as sets with one (a loop back to the
+        vertex) or two vertices.
+        """
+        edges = []
+        for vertex in self.__graph_dict:
+            for neighbor in self.__graph_dict[vertex]:
+                if {neighbor, vertex} not in edges:
+                    edges.append({vertex, neighbor})
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for key in self.__graph_dict:
+            res += str(key) + " "
+        res += "\nedges:"
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res
 
