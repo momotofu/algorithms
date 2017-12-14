64dd0c31b3df99c49757385c98d267fff644d8fb
Christopher Reece <mrkita.reece@gmail.com>
2017-12-14 20:25:24 +0900

    added slow soloution

--- sorting/array_left_rotation.py
+++ sorting/array_left_rotation.py
@@ -12,3 +12,10 @@ def array_left_rotation_generic(a, n, k):
     for _ in range(k):
         arr.rotate(-1)
     return arr
+
+def array_left_rotation_slow(a, n, k):
+    width = len(a)
+    for _ in range(k):
+        a.append(a[0])
+        a = a[1:]
+    return  a[:width//2] + a[width//2:]
