b97ee8f9b63f6fa16e1da67b9f8c0678fa8aca1f
Christopher Reece <mrkita.reece@gmail.com>
2017-12-14 20:24:26 +0900

    added generic python solution

--- sorting/array_left_rotation.py
+++ sorting/array_left_rotation.py
@@ -6,3 +6,9 @@ def array_left_rotation(a, n, k):
         new_index = (i + k) % n
         arr[i] = a[new_index]
     return arr
+
+def array_left_rotation_generic(a, n, k):
+    arr = deque(a, n)
+    for _ in range(k):
+        arr.rotate(-1)
+    return arr
