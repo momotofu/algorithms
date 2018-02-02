7efe78d8d20e2f6abfa34b71e036b17ed3984fe3
Christopher Reece <mrkita.reece@gmail.com>
2018-02-02 11:13:58 +0900

    added permutations.py

---
+++ math/permutations.py
@@ -0,0 +1,8 @@
+def permutations(string, step = 0):
+    if step == len(string):
+        return "".join(string))
+
+    for i in range(step, len(string)):
+        string_copy = [character for character in string]
+        string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
+        permutations(string_copy, step + 1)
